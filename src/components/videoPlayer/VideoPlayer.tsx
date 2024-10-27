3b2fd2e03c0b2f11a9384b8203f0c3b883f6e195
Murat Koyun <muratk1845@gmail.com>
2024-10-27 11:25:30 +0100

    videoPlayer waveform config

--- src/components/videoPlayer/VideoPlayer.tsx
+++ src/components/videoPlayer/VideoPlayer.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ReactPlayer from "react-player";
+import WaveSurfer from "wavesurfer.js";
 
 import { setSubtitle } from "../../store/subtitle/index";
 import { RootState } from "../../types";
@@ -9,11 +10,14 @@ import "./VideoPlayer.css";
 
 const VideoPlayer = () => {
   const [videoFilePath, setVideoFilePath] = useState<null | string>(null);
+  const [currentSubtitle, setCurrentSubtitle] = useState("");
   const dispatch = useDispatch();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const uploadedSubtitles = useSelector(
     (state: RootState) => state.subtitle.parsedSubtitles
   );
+  const waveSurferRef = useRef<WaveSurfer | null>(null);
+  const waveFormRef = useRef<string | HTMLElement>("");
+  const playerRef = useRef(null);
 
   const handleVideoUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const targetFile = (event.target as HTMLInputElement).files![0];
@@ -34,6 +38,28 @@ const VideoPlayer = () => {
     }
   };
 
+  const currentSubtitles = uploadedSubtitles.find((subtitle) => {
+    return (
+      playsSecond >= subtitle.startSeconds &&
+      playedSeconds <= subtitle.endSeconds
+    );
+  });
+  setCurrentSubtitle(currentSub ? currentSub.text : "");
+
+  useEffect(() => {
+    if (videoFilePath && waveFormRef) {
+      waveSurferRef.current = WaveSurfer.create({
+        container: waveFormRef.current,
+        waveColor: "#0693e3",
+        progressColor: "#333",
+        cursorColor: "#ff6347",
+        barWidth: 2,
+        height: 100,
+      });
+    }
+    
+  }, [videoFilePath]);
+
   return (
     <div className="videoPlayer-table">
       {videoFilePath ? (
@@ -42,6 +68,9 @@ const VideoPlayer = () => {
             <ReactPlayer
               className="video-player"
               url={videoFilePath}
+              onProgress={handleProgress}
+              onPlay={() => waveSurfer.current?.play()}
+              onPause={() => Wavesurfer.current?.pause()}
               width="100%"
               height="100%"
               controls
